6c16118e010b2e30cc1848c99ff95d04a21a9b0b
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-06-18 23:34:10 +0530

    :+1:

--- 4.ReactReduxModuleBundling/robofriends/src/SearchBox.jsx
+++ 4.ReactReduxModuleBundling/robofriends/src/SearchBox.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
-const SearchBox = ({searchChange}) => {
+import './index.css'
+const SearchBox = ({ searchChange }) => {
     return (
-        <div className='pa3'>
-            <input onChange={searchChange} className='pa3 ba b--green bg-lightest-blue' type='serach'
-                   placeholder='search robots'/>
+        <div className='pa3 searchBox'>
+            <input style={{ width: '50%',borderRadius:'10px'}} onChange={searchChange} className='pa3 bg-lightest-blue shadow-5' type='serach'
+                placeholder='search robots' />
         </div>
     )
 }
