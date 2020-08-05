eb5e65693015a1be2378e837cfbe88a43d850b03
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-08-05 09:57:54 +0530

    adding new font and bg color

--- 4.ReactReduxModuleBundling/robofriends/src/App.js
+++ 4.ReactReduxModuleBundling/robofriends/src/App.js
@@ -1,36 +1,43 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import {robots} from './Robots'
-class App extends Component  {
-    constructor() {
-        super();
-        this.state={
-            robots:robots,
-            searchFeild:""
-        }
-    }
-    onSearchChange=(event)=>{
-        this.setState({
-            searchFeild:event.target.value
-        })
-
-    }
-    render() {
-        const filterdRobots = this.state.robots.filter(robot=>robot.name.toLowerCase().includes(
-            this.state.searchFeild.toLowerCase()
-        ))
-        return (
-            <div className='tc' >
-                <h1 className='tc'>Robo Friends</h1>
-                <SearchBox searchFeild={this.state.searchFeild} searchChange={this.onSearchChange}/>
-                <CardList robots={filterdRobots}/>
-            </div>
-        )
-    }
-
-
-
+import { robots } from "./Robots";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchFeild: "",
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({
+      searchFeild: event.target.value,
+    });
+  };
+  render() {
+    const filterdRobots = this.state.robots.filter((robot) =>
+      robot.name.toLowerCase().includes(this.state.searchFeild.toLowerCase())
+    );
+    return (
+      <div className='tc'>
+        <h1
+          style={{
+            fontFamily: "Monoton",
+            color: "white",
+          }}
+          className='tc'
+        >
+          Robot Friends
+        </h1>
+        <SearchBox
+          searchFeild={this.state.searchFeild}
+          searchChange={this.onSearchChange}
+        />
+        <CardList robots={filterdRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
