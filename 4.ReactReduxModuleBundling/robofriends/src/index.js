d7738e1e51c4542df32c8a697988b8e4161ba947
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-08-07 09:03:34 +0530

    add redux logger :lock:

--- 4.ReactReduxModuleBundling/robofriends/src/index.js
+++ 4.ReactReduxModuleBundling/robofriends/src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-
-
-
 //css
 import "tachyons";
-//componenta
+
 import App from "./containers/App";
 import {reducer} from "./redux/reducer";
-const store = createStore(reducer);
+import {createLogger} from 'redux-logger';
+
+const logger = createLogger();
+const store = createStore(reducer,applyMiddleware(logger));
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store} >
