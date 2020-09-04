e4b7795115e34c6b2116ed966240e79b0662c798
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-09-04 08:08:48 +0530

    code splitting :cat2:

---
+++ 5.performace-part-2/code-splitting/src/components/page1.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from '../logo.svg';
+
+export const Page1 = ({onRouteChange})=>{
+    return(
+        <div className='App'>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <button className='btn disabled'>page1</button>
+            <button className='btn' onClick={()=>onRouteChange('page2')} >page2</button>
+            <button className='btn' onClick={()=>onRouteChange('page3')}>page3</button>
+        </div>
+    )
+}
