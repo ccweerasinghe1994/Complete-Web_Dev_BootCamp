46184e3a5a255543a068eb087122cc75248525e2
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-09-04 09:54:52 +0530

    more cleaner way of doing code splitting

--- 5.performace-part-2/code-splitting/src/App.js
+++ 5.performace-part-2/code-splitting/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Page1 from "./components/page1";
+import asyncComponent from "./components/AsyncComponent";
 
 
 class App extends Component {
@@ -9,38 +10,27 @@ class App extends Component {
         super(props, context);
         this.state = {
             route: 'page1',
-            component: ''
+            component: null
         }
     }
 
     onRouteChange = (route) => {
-        if (route === 'page1') {
-            this.setState({route})
-        }else if(route === 'page2'){
-            import('./components/page2').then((page2)=>{
-                console.log(page2)
-                this.setState({
-                    route:route,
-                    component:page2.default
-                })
-            })
-        }
-        else if(route === 'page3'){
-            import('./components/page3').then((page3)=>{
-                this.setState({
-                    route:route,
-                    component:page3.default
-                })
-            })
-        }
+        this.setState({
+            route:route
+        })
+
     }
 
     render() {
 
         if (this.state.route === 'page1') {
             return <Page1 onRouteChange={this.onRouteChange}/>
-        } else {
-            return <this.state.component onRouteChange={this.onRouteChange}/>
+        } else if (this.state.route === 'page2') {
+            const AsyncPage2 = asyncComponent(() => import('./components/page2'))
+            return <AsyncPage2 onRouteChange={this.onRouteChange}/>
+        } else if (this.state.route === 'page3') {
+            const AsyncPage3 = asyncComponent(() => import('./components/page3'))
+            return <AsyncPage3 onRouteChange={this.onRouteChange}/>
         }
     }
 
