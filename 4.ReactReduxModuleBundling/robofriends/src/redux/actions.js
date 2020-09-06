907cec521162970a5ff407335b6a1905dda95f6d
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-09-06 19:21:53 +0530

    Update actions.js

--- 4.ReactReduxModuleBundling/robofriends/src/redux/actions.js
+++ 4.ReactReduxModuleBundling/robofriends/src/redux/actions.js
@@ -13,7 +13,7 @@ export const requestRobots = () => (dispatch) => {
     dispatch({
         type: REQUEST_ROBOTS_PENDING
     });
-    fetch("http://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users",{})
         .then((response) => response.json())
         .then(data => dispatch({
                 type: REQUEST_ROBOTS_SUCCESS,
